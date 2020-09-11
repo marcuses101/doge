65e7a02bbe0ccd4a00d05c2f667bdcbc20db3b7e
marcuses101 <mnjconnolly@gmail.com>
2020-09-11 11:24:04 -0400

    README update. Clean code

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Doge
+
+## Description
+
+Get a random assortment of dog pictures or lookup a single photo of the breed of your choice.
+
+## Author Information
+Marcus Connolly  
+[LinkedIn](www.linkedin.com/in/marcus-connolly-web)  
+[Email](mailto:mnjconnolly@gmail.com)
+
+## Acknowledgments 
+
+Thanks to dogs everywhere for making the world a better place!
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const dogSlider = document.getElementById("numberOfDogs");
 const dogNumberSpan = document.getElementById("dogNumberDisplay")
-const dogURL = "https://dog.ceo/api/breeds/image/random/"
-const doggyDiv = document.getElementById("doggy-pics");
-const doggyList = doggyDiv.querySelector("ul");
+const doggyList = document.querySelector("ul");
 const randomForm = document.querySelector("#randomDogs");
 const breedForm = document.querySelector("#dogsByBreed")
 const loadGif = document.querySelector("#loading");
 const boneNav = document.querySelectorAll(".bone");
+const randomDogURL = "https://dog.ceo/api/breeds/image/random/";
 
-
+// return a promise that resolves to the image once loaded. 
+// reject returns false
 function generateImage(url) {
     return new Promise((resolve, reject) => {
         let image = new Image();
@@ -25,25 +25,39 @@ function generateImage(url) {
 
 async function getRandomDogs(num) {
     try {
-        if (!parseInt(num)) {
-            dogNumberSpan.textContent = "Come on! You must want at least one dog!"
-            return
-        }
+        // clear picture area. remove error pic/text if present
         doggyList.innerHTML = "";
         hideError();
+        // show loading dog
         loadGif.classList.remove("hide")
-        let response = await fetch(dogURL + num);
-        if (!response.ok) throw new Error(response.statusText)
+        
+        // fetch n dog urls
+        let response = await fetch(randomDogURL + num);
+
+        // convert response to usable object
         let data = await response.json();
-        console.log(data); // Assingment requirement 1
+        // throw error
+        if (!response.ok) throw new Error(`${data.code}: ${data.message}`)
+        // Assignment requirement 1 ✓
+        console.log(data); 
+        // wait for all the images to load
         let imgArray = await Promise.all(data.message.map(url => generateImage(url)))
-        doggyList.innerHTML = imgArray.map(image => {
-            if (image) return `<li>${image.outerHTML}</li>`
-        }).join("") + "<li></li>";
+        // abstract equality comparison! num is string
+        if (num == 1) {
+            // if one pup place directly for bigger format
+            doggyList.innerHTML = imgArray[0].outerHTML;
+        }
+        else {
+            // display images in list to facilitate layout
+            doggyList.innerHTML = imgArray.map(image => {
+                if (image) return `<li>${image.outerHTML}</li>`
+            }).join("") + "<li></li>" //prevent last photo from stretching;
+        }
+        // hide loading dog
         loadGif.classList.add("hide")
-        console.log("All dogs present and accounted for")
     }
     catch (error) {
+        // show error pup on failure
         loadGif.classList.add("hide")
         showError();
         console.log(error)
@@ -53,19 +67,29 @@ async function getRandomDogs(num) {
 
 async function getDogByBreed(breed){
     try{
+        //hide error message/image
         hideError();
+        //show loading gif
         loadGif.classList.remove("hide")
+        //clear images
         doggyList.innerHTML = "";
+        // fetch dogs by breed
         let response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-        console.log(response);
-        if (!response.ok) throw new Error(response.status);
+        // parse into javascript object
         let data = await response.json();
+        // if error throw error details
+        if (!response.ok) {throw new Error(`${data.code}: ${data.message}`)}
+        // load image
         let image = await generateImage(data.message);
+        // display image
         doggyList.innerHTML = image.outerHTML
+        // hide loading pup
         loadGif.classList.add("hide")
     }
     catch (e) {
+        // show error pup on failure
         showError();
+        // hide loading pup
         loadGif.classList.add("hide")
         console.log(e)
     }
@@ -81,22 +105,15 @@ function hideError() {
 }
 
 //Event Handlers
+
+// # of random dogs form submit
 function handleRandomSubmit(e) {
     e.preventDefault();
     console.log("Getting good boys")
     getRandomDogs(dogSlider.value)
 }
 
-function handleSlider(e) {
-    let num = parseInt(this.value);
-    dogNumberSpan.textContent =
-        num === 50
-            ? "50! So many good dogs 🐕!!!"
-            : num === 0
-                ? "0. What kind of monster wants zero good dogs?"
-                : num;
-}
-
+// dog by breed form submit
 function handleBreedSubmit(e) {
     e.preventDefault();
     let breed = this.querySelector("input[type=text]").value.toLowerCase();
@@ -104,6 +121,18 @@ function handleBreedSubmit(e) {
     console.log(breed);
 }
 
+// practicing cascading ternary operators
+function handleRangeSlider(e) {
+    let num = parseInt(this.value);
+    dogNumberSpan.textContent =
+        num === 50
+            ? "50! So many good dogs 🐕!!!"
+            : num === 1
+            ? "1. Really? Just one good dog?"
+            : num;
+}
+
+// handling navigation
 function handleBoneClick(e) {
     boneNav.forEach(bone=>{
         if (bone === this) {
@@ -118,8 +147,9 @@ function handleBoneClick(e) {
     document.querySelector(this.dataset.show).classList.remove("hide")
 }
 
+// initialize event handlers on page load
 function handleLoad() {
-    dogSlider.addEventListener("input", handleSlider)
+    dogSlider.addEventListener("input", handleRangeSlider)
     randomForm.addEventListener("submit", handleRandomSubmit)
     breedForm.addEventListener("submit", handleBreedSubmit)
     boneNav.forEach(bone => {
